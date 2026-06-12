1044033de6be692129458e74ebcab2736b5c49be
chlrlgns22-cgh <chlrlgns22@gmail.com>
2026-06-12 15:39:34 +0900

    Include wcfg waveform config files

--- 20260612_I2C_MASTER/20260612_I2C_MASTER.srcs/sources_1/new/I2C_Master.sv
+++ 20260612_I2C_MASTER/20260612_I2C_MASTER.srcs/sources_1/new/I2C_Master.sv
@@ -1,26 +1,31 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2026/06/12 15:28:38
-// Design Name: 
-// Module Name: I2C_Master
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module I2C_Master(
-
-    );
+module I2C_Master (
+    input logic clk,
+    input logic reset,
+    // command port
+    input logic cmd_start,
+    input logic cmd_write,
+    input logic cmd_read,
+    input logic cmd_stop,
+    // internal port
+    input logic [7:0] tx_data,
+    output logic [7:0] rx_data,
+    input logic ack_in,  // read 시 master가 보낼 ACK(0) / NACK(1)
+    output logic ack_out,  // write 시 slave로부터 받은 ACK(0)/NACK(1)
+    output logic busy,
+    output logic done,
+    // external i2c port
+    output logic scl,
+    output logic sda_o,
+    input logic sda_i
+);
+    typedef enum logic [2:0] {
+        IDLE = 3'b000,
+        START,
+        WAIT_CMD,
+        DATA,
+        DATA_ACK,
+        STOP
+    } i2c_state_e;
 endmodule
